5ac4f4b86689a20b58ae796cae6ace965513f208
Fraser <Fraser@frasermurray.org>
2024-03-10 18:28:20 +0300

    Help norn

--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -63,6 +63,9 @@
 #define MAP_UNDERWATER_ROUTE129 (54 | (0 << 8))
 #define MAP_UNDERWATER_ROUTE105 (55 | (0 << 8))
 #define MAP_UNDERWATER_ROUTE125 (56 | (0 << 8))
+#define MAP_KILOAVAS_REST       (57 | (0 << 8))
+#define MAP_KILOVAS_REST_S      (58 | (0 << 8))
+#define MAP_KILOAVAS_REST_E     (59 | (0 << 8))
 
 // gMapGroup_IndoorLittleroot
 #define MAP_LITTLEROOT_TOWN_BRENDANS_HOUSE_1F    (0 | (1 << 8))
@@ -400,6 +403,7 @@
 #define MAP_TERRA_CAVE_END                           (105 | (24 << 8))
 #define MAP_ALTERING_CAVE                            (106 | (24 << 8))
 #define MAP_METEOR_FALLS_STEVENS_CAVE                (107 | (24 << 8))
+#define MAP_NEW_MAP1                                 (108 | (24 << 8))
 
 // gMapGroup_IndoorDynamic
 #define MAP_SECRET_BASE_RED_CAVE1    (0 | (25 << 8))
